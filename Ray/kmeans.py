6ec4a6adcaca5b9f99933a6d4935a9fbca112907
LiagkaAikaterini <katerinaliaga1999@gmail.com>
2024-07-12 17:27:31 +0300

    trcemalloc

--- Ray/kmeans.py
+++ Ray/kmeans.py
@@ -2,6 +2,7 @@ import ray
 from ray.util import cluster
 from ray.util import graph
 import time
+import tracemalloc
 
 # Initialize Ray with 3 workers
 ray.init(num_cpus=3)
@@ -28,16 +29,24 @@ num_iterations = 100
 # Record start time
 start_time = time.time()
 
+# Start memory tracing
+tracemalloc.start()
+
 # Perform distributed k-means clustering using Ray
 centroids, cluster_assignments_ray = cluster.kmeans(adjacency_array_ray, num_clusters, num_iterations)
 
 # Record end time
 end_time = time.time()
 
+# End memory tracing
+currentMem, peakMem = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+
 # Print results
 print("Distributed K-Means Centroids (Ray):", centroids)
 print("Distributed K-Means Cluster Assignments (Ray):", cluster_assignments_ray)
 print("Time taken (Ray):", end_time - start_time, "seconds")
+print(f"Current memory usage is {currentMem / 10**6}MB; Peak was {peakMem / 10**6}MB")
 
 # Shutdown Ray
 ray.shutdown()
